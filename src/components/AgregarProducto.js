debcfb3361e367e931a3786177930d2aa619a68a
Emilse <emilsearias@gmail.com>
2021-04-19 17:27:50 -0300

    se agrego la validacion y el componente de error

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,27 +1,53 @@
 import React, { useState } from "react";
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Button, Alert } from "react-bootstrap";
 
 const AgregarProducto = () => {
   // crear states
   const [nombreProducto, setNombreProducto] = useState("");
   const [precioProducto, setPrecioProducto] = useState(0);
   const [categoria, setCategoria] = useState("");
+  const [error, setError] = useState(false);
 
-  const leerCategoria = (e) =>{
-    setCategoria(e.target.value)
-  }
+  const leerCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("prueba de submit");
+    // validaciones
+    if (
+      nombreProducto.trim() !== "" &&
+      precioProducto !== "" &&
+      categoria !== ""
+    ) {
+      // si esta bien entonces envio los datos del producto a la API
+      setError(false);
+    } else {
+      // si fallo entonces muestro un cartel de error
+      setError(true);
+    }
+  };
 
   return (
     <Container>
-      <Form className="my-5">
+      <Form className="my-5" onSubmit={handleSubmit}>
         <h1 className="my-5">Agregar nuevo producto</h1>
         <Form.Group>
           <Form.Label>Nombre del producto*</Form.Label>
-          <Form.Control type="text" placeholder="Café" onChange={(e)=> setNombreProducto(e.target.value)}></Form.Control>
+          <Form.Control
+            type="text"
+            placeholder="Café"
+            onChange={(e) => setNombreProducto(e.target.value)}
+          ></Form.Control>
         </Form.Group>
         <Form.Group>
           <Form.Label>Precio*</Form.Label>
-          <Form.Control type="number" placeholder="50" onChange={(e)=> setPrecioProducto(e.target.value)}></Form.Control>
+          <Form.Control
+            type="number"
+            placeholder="50"
+            onChange={(e) => setPrecioProducto(parseInt(e.target.value))}
+          ></Form.Control>
         </Form.Group>
         <h3 className="text-center mt-4">Categoria</h3>
         <div className="text-center my-4">
@@ -30,7 +56,7 @@ const AgregarProducto = () => {
             type="radio"
             label="Bebida caliente"
             name="categoria"
-            value='bebida-caliente'
+            value="bebida-caliente"
             onChange={leerCategoria}
           ></Form.Check>
           <Form.Check
@@ -38,7 +64,7 @@ const AgregarProducto = () => {
             type="radio"
             label="Bebida fria"
             name="categoria"
-            value='bebida-fria'
+            value="bebida-fria"
             onChange={leerCategoria}
           ></Form.Check>
           <Form.Check
@@ -46,7 +72,7 @@ const AgregarProducto = () => {
             type="radio"
             label="Dulce"
             name="categoria"
-            value='dulce'
+            value="dulce"
             onChange={leerCategoria}
           ></Form.Check>
           <Form.Check
@@ -54,10 +80,19 @@ const AgregarProducto = () => {
             type="radio"
             label="Salado"
             name="categoria"
-            value='salado'
+            value="salado"
             onChange={leerCategoria}
           ></Form.Check>
         </div>
+        <Button variant="danger" type="submit" className="w-100">
+          Guardar
+        </Button>
+        {
+          (error === true)?(<Alert variant="danger" className="my-5">
+          Faltan cargar datos obligatorios
+        </Alert>): null
+        }
+        
       </Form>
     </Container>
   );
