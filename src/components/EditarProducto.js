ebaee9aae55578202380bf0a34940322213c6679
Emilse <emilsearias@gmail.com>
2021-04-26 18:45:47 -0300

    se agrego la consulta a un producto dentro de editar Producto

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,8 +1,31 @@
 import React, {useState, useEffect} from "react";
 import { Container, Form, Button, Alert } from "react-bootstrap";
+import {useParams} from 'react-router-dom'
 import Swal from "sweetalert2";
 
 const EditarProducto = () => {
+  const [producto, setProducto] = useState({});
+  const URL =  process.env.REACT_APP_API_URL;
+  const {id} = useParams();
+ 
+  useEffect(()=>{
+    consultarProducto();
+  }, []);
+
+  const consultarProducto = async()=>{
+    try{
+        const respuesta = await fetch(URL+'/'+id);
+        console.log(respuesta);
+        if(respuesta.status === 200){
+           const productoEncontrado = await respuesta.json();
+           setProducto(productoEncontrado)
+        }
+    }catch(error){
+        console.log(error)
+        // mostrar un cartel al usuario
+    }
+  }
+
   const leerCategoria = (e) => {
     // setCategoria(e.target.value);
   };
